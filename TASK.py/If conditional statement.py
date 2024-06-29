c44428c67238fdacc76d7d990f3e3b0ef7c628f9
Sangeethasankar <sangeethasankar474@gmail.com>
2024-06-29 10:58:02 +0530

    if conditions

---
+++ TASK.py/If conditional statement.py
@@ -0,0 +1,21 @@
+#IF CONDITIONAL STATEMENT
+
+#string contains a specific substring
+q="good morning and welcome to the python class"
+w="python class"
+
+# if w in q:
+#     print ("w contains q")
+# else:
+#     print("w not in q")
+
+#number b\w 1 to 10
+s= 11
+# if s<1:
+#     print("s is lies between 1 to 10")
+# elif s>10:
+#     print("s is not lies between 1 to 10")
+
+#character is a vowels
+z=("a", "s","l")
+if z 
