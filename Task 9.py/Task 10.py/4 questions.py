e835a53e43976f990de2a0b571c882147d1f2f8b
Sangeethasankar1020 <sangeethasankar474@gmail.com>
2024-06-29 13:12:12 +0530

    list task

--- Task 9.py/Task 10.py/4 questions.py
+++ Task 9.py/Task 10.py/4 questions.py
@@ -1,9 +1,30 @@
-a=2
-b=3
-c=a+b
-print(c)
+# string methods
 
-txt="""Friendship is a relationship of mutual affection between people.
- It is a stronger form of interpersonal bond than an "acquaintance" or an "association",
-   such as a classmate, neighbor, coworker, or colleague."""
-print(txt.count("a"))
+# count
+# txt="""Friendship is a relationship of mutual affection between people.
+#  It is a stronger form of interpersonal bond than an "acquaintance" or an "association",
+#  such as a classmate, neighbor, coworker, or colleague."""
+# print(txt.count("a"))
+
+# email_id
+# w=input("enter email id:")
+# if w.endswith("_"):
+#     print("valid email_id")
+# else:
+#     print("invalid email_id")
+
+# password
+# q=input("enter a password:")
+# if q.isalnum():
+#     print("valid password")
+# else:
+#     print("invalid password")
+
+# email and password
+# email=input("enter email id:")
+# pw=input("enter password:")
+# if email.endswith("_") and pw.isalnum():
+#     print(f"login successfully")
+# else:
+#     print("invalid,try again")
+    
