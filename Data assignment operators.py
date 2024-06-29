e9c6a9e9cf418f9164b2e76fbc5357956050fc33
sujatha <sujasaravanan04@gmail.com>
2024-06-29 10:41:46 +0530

    operators

---
+++ Data assignment operators.py
@@ -0,0 +1,70 @@
+#print
+
+#equal
+a = 23
+s = a
+# print("equal", s)
+
+#+=
+x = 10
+x +=5
+# print(x)
+
+#-=
+t = 100
+t -= 50
+# print(t)
+
+#*=
+y = 25
+y *= 5
+# print(y)
+
+#/=
+s = 120
+s /= 20
+# print (s)
+
+#%=
+d = 20
+d %= 3  
+# print(d)
+
+#//=
+f = 25
+f //=5
+# print(f)
+
+#**=
+c = 3
+c **=2
+# print(c)
+
+#&=
+m = 4
+m &=6
+# print(m)
+
+#|=
+h = 5
+h |=2
+# print(h)
+
+#^=
+g = 8
+g ^=3
+# print(g)
+
+#<<=
+k = 5
+k <<=3
+# print(k)
+
+#>>=
+l = 9
+l >>=2
+# print(l)
+
+#:=
+
+print(m:=4)
