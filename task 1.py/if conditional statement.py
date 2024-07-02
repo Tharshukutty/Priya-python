47b8be46b22f548efc02618a70458cbe0f156e06
sujatha <sujasaravanan04@gmail.com>
2024-07-02 11:54:23 +0530

    typles and sets

--- task 1.py/if conditional statement.py
+++ task 1.py/if conditional statement.py
@@ -1,55 +1,55 @@
 #if conditional statement
 
 #string contains a substring
-# x=input("enter a string:")
-# y=input("enter a substring:")
-# if y in x:
-#     print("yes,y is substring of x")
-# else:
-#     print("no, y is substring of x")
+x=input("enter a string:")
+y=input("enter a substring:")
+if y in x:
+    print("yes,y is substring of x")
+else:
+    print("no, y is substring of x")
 
 #number between 1 to 10
-#n=int(input("enter number:"))
-# if n>1:
-#     print("n is lies in between 1 to 10",n)
-# elif n<10:
-#     print("n is lies between 1 to 10",n)
-# else:
-#     print("n is not in between 1 to 10")
+n=int(input("enter number:"))
+if n>1:
+    print("n is lies in between 1 to 10",n)
+elif n<10:
+    print("n is lies between 1 to 10",n)
+else:
+    print("n is not in between 1 to 10")
 
 #character is a vowels
 # x=str(input("enter a letter:"))
-# if x=="a"or x=="e"or x=="i"or x=="o"or x=="u":
+# if x=="a"or x=="e"or x=="i"or x=="o"or x=="u":S
 #     print("vowels")
 # else:
 #     print("not a vowels)"
 
 #eligible to vote
-# e=int(input("enter a number"))
-# if e>=18:
-#     print("eligible to vote",e)
-# else:
-#     print("not eligible to vote",e)
+e=int(input("enter a number"))
+if e>=18:
+    print("eligible to vote",e)
+else:
+    print("not eligible to vote",e)
 
 #leap year
-# s=int(input("enter a year:"))
-# if s%4==0 and s%100==0:
-#     print(s,"is a leap year")
-# elif s%400==0 and s%100!=0:
-#     print(s,"is a leap year")
-# else:
-#     print("its is not a leap year")
+s=int(input("enter a year:"))
+if s%4==0 and s%100==0:
+    print(s,"is a leap year")
+elif s%400==0 and s%100!=0:
+    print(s,"is a leap year")
+else:
+    print("its is not a leap year")
 
 #multiple of 3 and 7
-# n=int(input("enter a number:"))
-# if n%3==0:
-#     print("n is multiples of 3")
-# elif n%7==0:
-#     print("n is multiples of 7")
-# elif n%3!=0 and n%7!=0:
-#     print("n is not multiples of both 3 and 7")
-# else:
-#     print(" n is both multiples of 3 and 7")
+n=int(input("enter a number:"))
+if n%3==0:
+    print("n is multiples of 3")
+elif n%7==0:
+    print("n is multiples of 7")
+elif n%3!=0 and n%7!=0:
+    print("n is not multiples of both 3 and 7")
+else:
+    print(" n is both multiples of 3 and 7")
 
 #person is teenager
 
