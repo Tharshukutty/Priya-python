6f03209287d06517ddb9c763c4f20296a3734d72
srinath0025 <srinath2571997@gmail.com>
2024-08-02 11:26:26 +0530

    polymorphism

--- Oops/polymorphism.py
+++ Oops/polymorphism.py
@@ -39,3 +39,60 @@
 
 # objstu=student("tharshu","A")
 # objstu.display()
+# objstu=student("priya","B")
+# objstu.display()
+
+
+# create a base class called vehicle with a method start that print " vehicle started" 
+# class Vehicle:
+#     def start(self):
+#         print("Vehicle started")
+    
+# class Car(Vehicle):
+#     def start(self):
+#         print("Car started")
+
+# vehicle=Vehicle()
+# vehicle.start()
+# car=Car()
+# car.start()
+
+# create a base class called employee with properties name and salary & derived class called manager that inherits from employee and adds a property department 
+# class Employee:
+#     def __init__(self,name,salary):
+#         self.name=name 
+#         self.salary=salary
+# class Manager(Employee):
+#     def __init__(self,name,salary,department):
+#         super().__init__(name,salary)
+#         self.department=department
+#     def display(self):
+#         print(f"Name: {self.name},   Salary: {self.salary},    Department: {self.department}") 
+        
+# employee=Employee("Ashu",20000)
+# manager=Manager("Tharshu",50000,"HR")
+# manager.display()
+
+
+
+# Create a class called animal with a sound function that prints "Animal makes sounds" .
+# create a derived class called dog that inherits from animals and overrides the sounds methods to print "dog barks " .
+# create a another function derived class bird that inherits from animal and overrides sound method to print " birds print"
+
+# class Animal:
+#     def sound(self):
+#         print("Animal makes sound")
+# class Dog(Animal):
+#     def sound(self):
+#         print("Dog barks")
+# class Bird(Animal):
+#     def sound(self):
+#         print("Birds print")
+
+# animal=Animal()
+# dog=Dog()
+# bird=Bird()
+# dog.sound()
+# bird.sound()
+
+
